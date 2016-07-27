d8abd21549885ee4e7f4a825b7cd482bcf49b53b
Stephanie Shin <steph.shin2@gmail.com>
2016-07-27 17:00:40 +0200

    Onboarding

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
-var app = angular.module('chommiesApp', ['ngRoute']); 
+var app = angular.module('guestScanApp', ['ngRoute']); 
 
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
-		controller: 'FeedCtrl',
-		templateUrl: 'templates/feed.html',
+		controller: 'HomeCtrl',
+		templateUrl: 'templates/home.html',
+	})
+	$routeProvider.when('/signup', {
+		controller: 'SignUpCtrl',
+		templateUrl: 'templates/signup.html',
+	})
+	$routeProvider.when('/bmprofile', {
+		controller: 'bmProfileCtrl',
+		templateUrl: 'templates/bm/bmprofile.html',
+	})
+	$routeProvider.when('/tenantprofile', {
+		controller: 'tenantProfileCtrl',
+		templateUrl: 'templates/tenant/tenantprofile.html',
+	})
+	$routeProvider.when('/securityprofile', {
+		controller: 'securityProfileCtrl',
+		templateUrl: 'templates/security/securityprofile.html',
+	})
+	$routeProvider.when('/visitorprofile', {
+		controller: 'visitorProfileCtrl',
+		templateUrl: 'templates/visitor/visitorprofile.html',
+	})
+
+
+
+	$routeProvider.when('/bm/:uid', {
+		controller: 'bmProfileCtrl',
+		templateUrl: 'templates/bm/bmprofile.html',
+	})
+	$routeProvider.when('/tenant/:uid', {
+		controller: 'tenantProfileCtrl',
+		templateUrl: 'templates/tenant/tenantprofile.html',
+	})
+	$routeProvider.when('/security/:uid', {
+		controller: 'securityProfileCtrl',
+		templateUrl: 'templates/security/securityprofile.html',
+	})
+	$routeProvider.when('/visitor/:uid', {
+		controller: 'visitorProfileCtrl',
+		templateUrl: 'templates/visitor/visitorprofile.html',
 	})
 
 });
 
-app.controller('FeedCtrl', function($scope, $http) {
-	$http({
-	    method: "GET",
-	    url: "http://ixchommies.herokuapp.com/props",
-	    params: {
-       		token: "be9378d11b18ca40c4823c7cb7fb9813"
-	    }
-	  }).then(function(response) {
-	  
-	  	$scope.props = response.data;
-	  })
+app.controller('HomeCtrl', function($scope, $http) {
 
+
+
+	$scope.signin = function() {
+		console.log("signed in!");
+
+
+		$location.path("/$scope.usertype/$firebase.uid")
 	}
-});
 
 
 
+	});
 
+app.controller('SignUpCtrl', function($scope, $http) {
+	
+
+	});
+
+
+app.controller('bmProfileCtrl', function($scope, $http) {
+	
+
+	});
+
+app.controller('tenantProfileCtrl', function($scope, $http) {
+	
+
+	});
+
+app.controller('securityProfileCtrl', function($scope, $http) {
+	
+
+	});
+
+app.controller('visitorProfileCtrl', function($scope, $http) {
+	
+
+	});
 
